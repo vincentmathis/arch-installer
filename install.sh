a98c077befaafb9133d4ddbf108956f3f459fb54
Vincent Mathis <vincemathis94@gmail.com>
2020-03-17 19:14:03 +0100

    Personal fresh arch install bash script WIP

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+sudo pacman -S keepassxc nextcloud-client
+
+cd "$HOME"
+
+### install yay AUR helper
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+rm -rf yay
+
+### install yadm
+yay -S --answerclean y --answerdiff n yadm-git
+
+### clone dotfiles
+yadm clone --bootstrap https://github.com/vincentmathis/dotfiles.git
+yadm drop stash
+yadm decrypt
+
+# https://github.com/MatMoul/archfi
+# https://github.com/MatMoul/archdi
+# https://github.com/tom5760/arch-install
