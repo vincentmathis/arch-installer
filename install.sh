5b39011296248218b0b3aed53e5a88fe0a26f83f
Vincent Mathis <vincentmathis@protonmail.com>
2022-10-29 00:28:37 +0200

    Updated script and added dreds in .env file

--- install.sh
+++ install.sh
@@ -1,26 +1,30 @@
 #!/bin/sh
 
-sudo pacman -S --noconfirm --needed base-devel
-
-cd "$HOME"
+sudo pacman -S --noconfirm --needed base-devel yadm
 
 ### install yay AUR helper
-git clone https://aur.archlinux.org/paru.git
-cd paru
+cd "$HOME"
+git clone https://aur.archlinux.org/yay.git
+cd yay
 makepkg --noconfirm -si
 cd ..
-rm -rf paru
+rm -rf yay
 
-### install yadm
-sudo pacman -S --noconfirm yadm
+### source github creds from cfg or prompt user
+SCRIPT=$(realpath "$0")
+FILE=$(dirname "$SCRIPT")'/.env'
+if [ -f $FILE ]; then
+    source $FILE
+else
+    read -p 'GitHub Username: ' GITHUB_USER
+    read -sp 'GitHub Access Token: ' GITHUB_TOKEN
+fi
 
 ### clone dotfiles
-yadm clone --bootstrap https://github.com/vincentmathis/dotfiles.git
+yadm clone --bootstrap https://$GITHUB_USER:$GITHUB_TOKEN@github.com/vincentmathis/dotfiles.git
+
+### decrypt ssh key
 yadm decrypt
 
-### change yadm to ssh after dedrypting ssh keys
+### change yadm to ssh
 yadm remote set-url origin git@github.com:vincentmathis/dotfiles.git
-
-# https://github.com/MatMoul/archfi
-# https://github.com/MatMoul/archdi
-# https://github.com/tom5760/arch-install
